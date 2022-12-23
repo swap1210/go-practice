820c71607d19efaacf245ee4f53dcdab47433a2a
Swapnil Patel <p.swap1210@gmail.com>
2022-12-23 07:09:15 -0600

    simplified without data structure

--- competative/leetcode/dailyTemperatures.go
+++ competative/leetcode/dailyTemperatures.go
@@ -5,52 +5,23 @@ import "fmt"
 func main() {
 	fmt.Println(dailyTemperatures([]int{73, 74, 75, 71, 69, 72, 76, 73}))
 }
-
-type MyStack struct {
-	arr []int
-}
-
-func (o *MyStack) pop() int {
-	if o.isEmpty() {
-		return -1
-	}
-	t := o.arr[0]
-	o.arr = o.arr[1:]
-	return t
-}
-
-func (o *MyStack) peek() int {
-	if o.isEmpty() {
-		return -1
-	}
-	return o.arr[0]
-}
-
-func (o *MyStack) push(i int) {
-	o.arr = append([]int{i}, o.arr...)
-}
-
-func (o *MyStack) isEmpty() bool {
-	return len(o.arr) == 0
-}
-
 func dailyTemperatures(temperatures []int) []int {
-	l := len(temperatures)
-	if l < 2 {
-		return []int{0}
-	}
-
-	s := &MyStack{arr: []int{l - 1}}
-	ans := make([]int, l)
-	for i := l - 2; i > -1; i-- {
-
-		for !s.isEmpty() && temperatures[s.peek()] <= temperatures[i] {
-			s.pop()
+	ans := make([]int, len(temperatures))
+	stack := make([]int, 0)
+	for i, t := range temperatures {
+		if len(stack) == 0 {
+			stack = append(stack, i)
+			continue
 		}
-		if !s.isEmpty() && temperatures[i] < temperatures[s.peek()] {
-			ans[i] = s.peek() - i
+		if t > temperatures[stack[len(stack)-1]] {
+			for len(stack) > 0 &&
+				t > temperatures[stack[len(stack)-1]] {
+				ans[stack[len(stack)-1]] =
+					i - stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+			}
 		}
-		s.push(i)
+		stack = append(stack, i)
 	}
 	return ans
 }
